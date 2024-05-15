406cfc6fe88e3df0134455379df7e375ddccd1e4
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-05-15 21:09:02 +0530

    changes

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -10,14 +10,13 @@ from facefusion import metadata, wording
 
 TORCH : Dict[str, str] =\
 {
-	'default': 'default',
-	'cpu': 'cpu',
-	'cuda': 'cu118',
-	'rocm': 'rocm5.6'
+	'cpu': 'https://download.pytorch.org/whl/cpu',
+	'cuda': 'https://download.pytorch.org/whl/cu118',
+	'rocm': 'https://download.pytorch.org/whl/rocm5.6'
 }
 ONNXRUNTIMES : Dict[str, Tuple[str, str]] =\
 {
-	'default': ('onnxruntime', '1.16.0'),
+	'cpu': ('onnxruntime', '1.16.0 '),
 	'cuda': ('onnxruntime-gpu', '1.16.0'),
 	'coreml-legacy': ('onnxruntime-coreml', '1.13.1'),
 	'coreml-silicon': ('onnxruntime-silicon', '1.16.0'),
@@ -59,13 +58,11 @@ def run(program : ArgumentParser) -> None:
 		])
 	if answers is not None:
 		torch = answers['torch']
-		torch_wheel = TORCH[torch]
+		torch_url = TORCH[torch]
 		onnxruntime = answers['onnxruntime']
 		onnxruntime_name, onnxruntime_version = ONNXRUNTIMES[onnxruntime]
 		subprocess.call([ 'pip', 'uninstall', 'torch', '-y' ])
-		if torch_wheel == 'default':
-			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt' ])
-		else:
-			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', 'https://download.pytorch.org/whl/' + torch_wheel ])
-		subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
+		subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', torch_url ])
+		if onnxruntime != 'cpu':
+			subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
 		subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version ])
